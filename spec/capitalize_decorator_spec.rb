520cdfac42533fc6ab605829db5b31d1f95d7edf
ram1117 <ramkumark1@outlook.com>
2023-04-19 18:11:29 +0530

    add unit test cases for decorators, rental class. update readme file

---
+++ spec/capitalize_decorator_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../modules/decorators/capitalize_decorator'
+
+describe CapitalizeDecorator do
+  before :each do
+    @nameable_obj = double('nameable object')
+    allow(@nameable_obj).to receive(:correct_name) { 'martin luther' }
+  end
+
+  it 'should create an instance of CapitalizeDecorator' do
+    cd = CapitalizeDecorator.new(@nameable_obj)
+    expect(cd).to be_instance_of CapitalizeDecorator
+  end
+  it 'should create instance of type BaseDecorator' do
+    cd = CapitalizeDecorator.new(@nameable_obj)
+    expect(cd).to be_kind_of BaseDecorator
+  end
+  it 'correct_name method should return a capitalized string' do
+    cd = CapitalizeDecorator.new(@nameable_obj)
+    puts cd.correct_name
+    expect(cd.correct_name).to eql 'Martin luther'
+  end
+end
